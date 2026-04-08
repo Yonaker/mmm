7ecba5142312545fae57a3604dbdb6c8497e50ac
Yonaker <yonakersart@umes.edu.gt>
2026-04-08 17:59:36 -0600

    commit parte_yonaker

--- parte_yonaker.cpp
+++ parte_yonaker.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-void mostrarReglas() {
+void mostrar_reglas() {
     cout << "\n=== REGLAS DEL JUEGO ===\n";
     cout << "1) Existen 3 filas con 3, 5 y 7 objetos.\n";
     cout << "2) Cada jugador debe retirar objetos de una sola fila.\n";
@@ -10,9 +10,14 @@ void mostrarReglas() {
     cout << "4) El jugador que deje exactamente 1 objeto pierde.\n";
     cout << "5) Si no quedan objetos, se considera empate.\n";
 }
-
+void mostrar_filas(int f1, int f2, int f3){
+    cout <<"Estado actual: ";
+    cout <<"Fila 1"<<f1<<endl;
+    cout <<"Fila 2"<<f2<<endl;
+    cout <<"Fila 3"<<f3<<endl;
+}
 int main() {
-    int opcionMenu;
+    int opcion_menu;
     bool activo = true;
 
     while (activo) {
@@ -21,19 +26,35 @@ int main() {
         cout << "2) Ver reglas\n";
         cout << "3) Finalizar\n";
         cout << "Ingrese su opcion: ";
-        cin >> opcionMenu;
+        cin >> opcion_menu;
 
-        switch (opcionMenu) {
+        switch (opcion_menu) {
             case 1:
+                int f1 = 3, f2 = 5, f3 = 7;
+                int Fila, Cantidad;
+                int jugador = 1;
+
+                while (true){
+                    mostrar_filas(f1, f2, f3);
+                    int total= f1 + f2 +3;
+                    
+                    if(total == 0){
+                        cout<<"empate, no quedan mas numeros en las filas";
+                        break;
+                    }
+                    if(total == 1){
+                        cout<<"jugador"<<jugador<<"pierde por dejar 1";
+                    }
+
                 cout << "La partida iniciará pronto...\n";
                 break;
-
+                }
             case 2:
-                mostrarReglas();
+                mostrar_reglas();
                 break;
 
             case 3:
-                cout << "Cerrando el programa. ¡Hasta luego!\n";
+                cout << "Cerrando el programa. ¡Hasta luego! ING SISTEMAS\n";
                 activo = false;
                 break;
 
